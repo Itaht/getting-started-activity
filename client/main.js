205f098c419eecb8f8cfbb5fbaa04e8712cbba24
ltaht <specter15564@gmail.com>
2025-01-04 01:57:26 +0700

    Deploy updated dist files

--- client/main.js
+++ client/main.js
@@ -1,6 +1,3 @@
-// Import CSS
-import './style.css';
-
 // Simulate running inside Discord by overriding the navigator.userAgent for development
 if (import.meta.env.MODE === "development") {
   Object.defineProperty(navigator, "userAgent", {
@@ -10,8 +7,8 @@ if (import.meta.env.MODE === "development") {
   console.log("Modified userAgent for development:", navigator.userAgent);
 }
 
-// Import the Discord SDK
 import { DiscordSDK } from "@discord/embedded-app-sdk";
+import "./style.css";
 
 // Extract query parameters from the URL
 const urlParams = new URLSearchParams(window.location.search);
