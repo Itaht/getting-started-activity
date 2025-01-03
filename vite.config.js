25fba5e78d4378fdd78f3d6e50c26590441e8bcd
ltaht <specter15564@gmail.com>
2025-01-04 01:27:00 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/getting-started-activity/' : '/',
+  server: {
+    port: 5173, // เปลี่ยนพอร์ตเป็น 3000
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
-      input: './client/index.html', // Make sure this path is correct
+      input: './client/index.html',
     },
-    assetsInlineLimit: 0,
   },
 });
