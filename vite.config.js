a1bf083e56072c2b8c94eec6a8c802b2d3c04566
ltaht <specter15564@gmail.com>
2025-01-04 02:05:02 +0700

    Deploy updated project

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/getting-started-activity/' : '/',
-  server: {
-    port: 5173, // เปลี่ยนพอร์ตเป็น 3000
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
-      input: './client/index.html',
+      input: './client/index.html', // Make sure this path is correct
     },
+  },
+  server: {
+    port: 5173,
+    open: true, // Auto-open the browser
   },
 });
